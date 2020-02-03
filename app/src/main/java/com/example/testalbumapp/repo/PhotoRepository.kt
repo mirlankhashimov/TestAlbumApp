8f8a61060cac5277c0ebf217e70104c6278303ff
Mirlan <47585674+mirlankhashimov@users.noreply.github.com>
2020-02-03 21:57:30 +0600

    added test class

--- app/src/main/java/com/example/testalbumapp/repo/PhotoRepository.kt
+++ app/src/main/java/com/example/testalbumapp/repo/PhotoRepository.kt
@@ -1,16 +1,97 @@
-package com.example.testalbumapp.repo
+package com.toiapp.android.feature.services.places.repository
 
-import com.example.testalbumapp.db.GalleryDatabase
-import com.example.testalbumapp.domain.datasource.photos.PhotoRemoteDataSource
-import com.example.testalbumapp.domain.datasource.resultLiveData
+import androidx.lifecycle.LiveData
+import androidx.paging.PagedList
+import androidx.paging.toLiveData
+import com.toiapp.android.core.extensions.AppPreferences
+import com.toiapp.android.core.extensions.load
+import com.toiapp.android.core.extensions.parentScope
+import com.toiapp.android.core.model.Image
+import com.toiapp.android.feature.services.places.database.PlacesDatabase
+import com.toiapp.android.feature.services.places.model.*
+import com.toiapp.android.feature.services.places.network.PlacesApi
+import com.toiapp.android.feature.services.places.network.PlacesBoundaryCallback
+import kotlinx.coroutines.CoroutineScope
+import okhttp3.MediaType
+import okhttp3.RequestBody
+import timber.log.Timber
+import java.io.File
 
-class PhotoRepository(
-    private val db: GalleryDatabase,
-    private val photoRemoteDataSource: PhotoRemoteDataSource
+class PlacesRepository(
+    private val placesDatabase: PlacesDatabase,
+    private val placesApi: PlacesApi,
+    private val currentUserId: String,
+    private val preferences: AppPreferences
 ) {
-    fun photos(id:Int) = resultLiveData(
-        databaseQuery = {db.photoDao().getPhotos()},
-        networkCall = {photoRemoteDataSource.fetchData(id)},
-        saveCallResult = {db.photoDao().insertAll(it)}
-    )
+    var placesBoundaryCallback: PlacesBoundaryCallback? = null
+
+    fun getPlaces(
+        coroutineScope: CoroutineScope,
+        filter: LiveData<Filter?>,
+        onlyFavorites: Boolean
+    ): LiveData<PagedList<Place>> {
+        placesBoundaryCallback = PlacesBoundaryCallback(10, coroutineScope, filter, onlyFavorites)
+        return placesDatabase
+            .placeDao()
+            .run {
+                when {
+                    onlyFavorites -> getAllFavorites()
+                    preferences.isVendor -> getMyPlaces(currentUserId)
+                    else -> getAll()
+                }
+            }
+            .toLiveData(10, boundaryCallback = placesBoundaryCallback)
+    }
+
+    fun invalidatePlaces() {
+        placesBoundaryCallback?.invalidate()
+    }
+
+    suspend fun deleteAllPlaces() {
+        placesDatabase.placeDao().deleteAll()
+    }
+
+    suspend fun favourite(place: Place) {
+        val response = placesApi.favorite(
+            FavoriteRequest(
+                currentUserId,
+                place.id
+            )
+        )
+        if (response.isSuccessful) {
+            val newPlace = place.copy(inFavorites = !place.inFavorites)
+            placesDatabase.placeDao().insert(listOf(newPlace))
+        }
+    }
+
+    suspend fun getPlace(id: Int): LiveData<Place> = parentScope {
+        load {
+            val place = placesApi.getPlace(id)
+            place.galleryList = placesApi.getImages(id)
+            val localPlace = placesDatabase.placeDao().getPlaceSync(id)
+            val newPlace = place.copy(indexInResponse = localPlace?.indexInResponse ?: place.indexInResponse)
+            placesDatabase.placeDao().insert(listOf(newPlace))
+        }
+        placesDatabase.placeDao().getPlace(id)
+    }
+
+    suspend fun getTypes(): LiveData<List<Type>> = parentScope {
+        load {
+            val types = placesApi.getTypes()
+            placesDatabase.typeDao().insert(types)
+        }
+        placesDatabase.typeDao().getAllLive()
+    }
+
+    suspend fun getKitchens(): LiveData<List<Cuisine>> = parentScope {
+        load {
+            val remoteTypes = placesApi.getKitchens()
+            placesDatabase.kitchenDao().deleteAllAndInsert(remoteTypes)
+        }
+        placesDatabase.kitchenDao().getAllLive()
+    }
+
+    suspend fun add(place: Place): Int = placesApi.addPlace(place).id
+    suspend fun update(place: Place) = placesApi.updatePlace(place).isSuccessful
+    suspend fun save(place: Place, images: List<Image>) = placesApi.save(place, images, images)
 }
